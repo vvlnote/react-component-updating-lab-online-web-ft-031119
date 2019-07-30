8dc3a3560d3cb7f5b3f45c9976982bbbe3af84d1
Joy Lin <vvlnote@hotmail.com>
2019-07-30 22:02:41 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -24,7 +24,7 @@ class Timer extends Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16); 
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
 
